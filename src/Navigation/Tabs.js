0e16000c0739f54c0e8e0b2f9f631141aecf5a43
Rakib Hossen <rakibhossen1660@gmail.com>
2023-08-06 11:48:06 +0600

    create image and functional bottom

--- src/Navigation/Tabs.js
+++ src/Navigation/Tabs.js
@@ -6,6 +6,7 @@ import ScreenTwo from '../Screens/ScreenTwo';
 import ScreenThree from '../Screens/ScreenThree';
 import ScreenFour from '../Screens/ScreenFour';
 import Education from '../Screens/Education';
+import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,11 +14,116 @@ export default class Tabs extends Component {
   render() {
     return (
       <Tab.Navigator>
-        <Tab.Screen name='Screen One' component={ScreenOne} />
-        <Tab.Screen name='Screen Two' component={ScreenTwo} />
-        <Tab.Screen name='Screen Three' component={ScreenThree} />
-        <Tab.Screen name='Screen Four' component={ScreenFour} />
-        <Tab.Screen name='Screen Five' component={Education} />
+        <Tab.Screen name='Screen One' component={ScreenOne} 
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: focused ? '#dce2ed' : 'transparent',
+              borderRadius: 5,
+            }}>
+              <Image 
+              source={require('../images/home.png')}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#2a52be' : '#000',
+                margin: 10,
+              }}
+              />
+            </View>
+          )
+        }} />
+        <Tab.Screen name='Screen Two' component={ScreenTwo} 
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: focused ? '#dce2ed' : 'transparent',
+              borderRadius: 5,
+            }}>
+              <Image 
+              source={require('../images/hexagon.png')}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#2a52be' : '#000',
+                margin: 10,
+              }}
+              />
+            </View>
+          )
+        }} />
+        <Tab.Screen name='Screen Three' component={ScreenThree} 
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: focused ? '#dce2ed' : 'transparent',
+              borderRadius: 5,
+            }}>
+              <Image 
+              source={require('../images/box.png')}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#2a52be' : '#000',
+                margin: 10,
+              }}
+              />
+            </View>
+          )
+        }} />
+        <Tab.Screen name='Screen Four' component={ScreenFour} 
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: focused ? '#dce2ed' : 'transparent',
+              borderRadius: 5,
+            }}>
+              <Image 
+              source={require('../images/heart.png')}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#2a52be' : '#000',
+                margin: 10,
+              }}
+              />
+            </View>
+          )
+        }} />
+        <Tab.Screen name='Screen Five' component={Education} 
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: focused ? '#dce2ed' : 'transparent',
+              borderRadius: 5,
+            }}>
+              <Image 
+              source={require('../images/settings.png')}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#2a52be' : '#000',
+                margin: 10,
+              }}
+              />
+            </View>
+          )
+        }} />
       </Tab.Navigator>
     )
   }
