23c980e1dfb42380281c2487780ec049e7802567
Rakib Hossen <rakibhossen1660@gmail.com>
2023-08-06 11:25:58 +0600

    tab navigation create

--- src/Navigation/Tabs.js
+++ src/Navigation/Tabs.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import ScreenOne from '../Screens/ScreenOne';
+import ScreenTwo from '../Screens/ScreenTwo';
+import ScreenThree from '../Screens/ScreenThree';
+import ScreenFour from '../Screens/ScreenFour';
+import Education from '../Screens/Education';
 
 const Tab = createBottomTabNavigator();
 
 export default class Tabs extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Tab.Navigator>
+        <Tab.Screen name='Screen One' component={ScreenOne} />
+        <Tab.Screen name='Screen Two' component={ScreenTwo} />
+        <Tab.Screen name='Screen Three' component={ScreenThree} />
+        <Tab.Screen name='Screen Four' component={ScreenFour} />
+        <Tab.Screen name='Screen Five' component={Education} />
+      </Tab.Navigator>
     )
   }
 }
---
+++ src/Screens/ScreenOne.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class ScreenOne extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Screen One </Text>
+      </View>
+    )
+  }
+}
+
+export default ScreenOne
